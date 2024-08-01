6bafef4cbdc1c29cecc3ad4d7c4d8ba782fa753c
Patrick Weppelmann <patrick.weppelmann+KBGroup@neoimpulse.de>
2024-08-01 18:03:39 +0200

    feat: Added configuration

--- release.config.cjs
+++ release.config.cjs
@@ -1,23 +1,3 @@
 module.exports = {
-    branches: ["main", { name: "next", prerelease: true }],
-    plugins: [
-        [
-            "@semantic-release/commit-analyzer",
-            {
-                preset: "default",
-                releaseRules: [
-                    { type: "feat", release: "minor" },
-                    { type: "fix", release: "patch" },
-                    { type: "docs", release: false },
-                    { type: "style", release: false },
-                    { type: "refactor", release: false },
-                    { type: "perf", release: "patch" },
-                    { type: "test", release: false }
-                ]
-            }
-        ],
-        "@semantic-release/release-notes-generator",
-        "@semantic-release/npm",
-        "@semantic-release/github"
-    ]
+    branches: ["main", { name: "next", prerelease: true }]
 };
